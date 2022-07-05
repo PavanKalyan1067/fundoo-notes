a57e127653459ea98add53fb7f50f679d99b90e6
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-06 03:27:13 +0530

    added response codes for notes

---
+++ FundooNotes/Fundoonotes/Response.py
@@ -0,0 +1,33 @@
+
+response_code = {
+    200: "Success",
+    201: "Created",
+    202: "Updated",
+    203: "Ok",
+    300: "Validation error",
+    301: "Error occurred while sending activation email",
+    302: "User account is notready activated",
+    303: "Email user does not exists",
+    304: "Token Expired",
+    305: "Error occur while redirecting",
+    306: "Insufficient data",
+    307: "You do not have any notes!",
+    308: "errors",
+    401: "Username already exists",
+    403: "Password didnt matched",
+    404: "Provide valid email",
+    405: "Invalid data",
+    406: "Please enter alpha numeric, atleast 8 digit password",
+    407: "Token does not exists",
+    408: "Email already exists in registered user",
+    409: "Does not exists",
+    410: "Logout first",
+    411: "Verify email first",
+    412: "Wrong password",
+    413: "Login first",
+    414: "Invalid operation",
+    415: "Enter upcoming time",
+    416: "Something went wrong. Please Try again"
+
+}
+
