a477a3ee31663fd50695bba6b4a7753c39878ded
PavanKalyan <pavankalyanyadav321@gmail.com>
2022-07-05 16:58:59 +0530

    Added urls for users

--- FundooNotes/users/urls.py
+++ FundooNotes/users/urls.py
@@ -1,19 +1,26 @@
-
 from django.urls import path
-from users.views import RegisterView, LogoutAPIView, SetNewPasswordAPIView, VerifyEmail, LoginAPIView, RequestPasswordResetEmail
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
+from users.views import (
+    RegisterView,
+    LogoutAPIView,
+    VerifyEmail,
+    LoginAPIView,
+    SetNewPasswordAPIView,
+    PasswordResetEmailAPIView,
+    UserProfileView,
+ForgotPasswordResetAPIView,
 )
-
+from rest_framework_simplejwt.views import (TokenRefreshView, )
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
     path('login/', LoginAPIView.as_view(), name="login"),
     path('logout/', LogoutAPIView.as_view(), name="logout"),
     path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
+    path('forgot-password/', ForgotPasswordResetAPIView.as_view(), name='forgot-password-reset'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('request-reset-email/', RequestPasswordResetEmail.as_view(),
-         name="request-reset-email"),
-    path('password-reset-complete', SetNewPasswordAPIView.as_view(),
-         name='password-reset-complete')
+    path('password-reset-complete/', PasswordResetEmailAPIView.as_view(),
+         name='password-reset-complete'),
+    path('reset-password/<uid>/<token>/', SetNewPasswordAPIView.as_view(), name='reset-password'),
+    path('get-all-users/', UserProfileView.as_view(), name="all_users"),
+
 ]
