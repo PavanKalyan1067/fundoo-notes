0ff14f1a5c697d9d2be2a11969e7973f44020131
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-06 02:34:34 +0530

    Modified CURD Operations in Notes

--- FundooNotes/Fundoonotes/views.py
+++ FundooNotes/Fundoonotes/views.py
@@ -1,8 +1,13 @@
+import datetime
+from datetime import timedelta
+
+from django.utils import timezone
 from rest_framework import serializers, status
 from django.shortcuts import render
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
+from users.models import User
 from .logger import get_logger
 from .models import Notes
 
@@ -10,49 +15,58 @@ from .models import Notes
 from rest_framework.views import APIView
 
 from .serializers import NotesSerializer
+from .utils import get_notes_by_user_id
+from rest_framework.exceptions import APIException
 
 logger = get_logger()
+from datetime import datetime
+from rest_framework.exceptions import PassedTimeException
+
+from Fundoonotes.Response import validate_time, response_code
 
 
 class RetrieveAPIView(APIView):
     def get(self, request):
         try:
             data = Notes.objects.all()
+            logger.info('Getting the notes data on %s', timezone.now())
             serializer = NotesSerializer(data, many=True)
             serialized_data = serializer.data
             response = {
                 'success': True,
+                'message': response_code[200],
                 'data': serialized_data,
-                'message': 'Successfully Retrieved all Data!',
             }
-            return Response(response, status=status.HTTP_200_OK)
+            return Response(response, )
         except Exception as e:
             response = {
                 'success': False,
-                'message': 'Oops! Something went wrong! Please try again...'
+                'message': response_code[416],
             }
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            return Response(response)
 
 
 class CreateAPIView(APIView):
-    def post(self, request, user_id):
+
+    def post(self, request):
         try:
             serializer = NotesSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-            notes = Notes(title=serializer.data.get('title'), description=serializer.data.get('description'),
-                          user_id=user_id)
+            notes = Notes(title=serializer.data.get('title'), description=serializer.data.get('description'))
             notes.save()
             data = serializer.data
-            # data.update({'id': notes.id})
             response = {
                 'success': True,
-                'message': 'Notes created successfully!',
+                'message': response_code[201],
                 'data': {'notes_list': data}
-            }
-            return Response(response, status=status.HTTP_201_CREATED)
+            },
+            return Response(response)
         except Exception as e:
-            return Response({'success': False, 'message': 'Oops! Something went wrong!'},
-                            status=status.HTTP_400_BAD_REQUEST)
+            response = {
+                'success': False,
+                'message': response_code[416],
+            }
+            return Response(response)
 
 
 class UpdateAPIView(APIView):
@@ -62,32 +76,34 @@ class UpdateAPIView(APIView):
             serializer = NotesSerializer(data, data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            serialized_data = serializer.data
+            # serialized_data = serializer.data
             response = {
-                'status': 'Successfully Updated Data',
-                'user': serialized_data,
+                'success': True,
+                'message': response_code[202],
+                'data': {'notes_list': serializer.data},
             }
-            return Response(response, status=status.HTTP_202_ACCEPTED)
+            return Response(response)
         except Exception as e:
             response = {
                 'success': False,
-                'message': 'Oops! Something went wrong! Please try again...'
+                'message': response_code[416],
             }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        return Response(response)
 
 
 class DeleteAPIView(APIView):
     def delete(self, request, ):
         try:
-            data = Notes.objects.get(pk=9)
+            data = Notes.objects.get(pk=2)
             data.delete()
             response = {
-                'status': 'Successfully Deleted Data',
+                'success': True,
+                'message': response_code[200],
             }
-            return Response(response, status=status.HTTP_202_ACCEPTED)
+            return Response(response)
         except Exception as e:
             response = {
                 'success': False,
-                'message': 'Oops! Something went wrong! Please try again...'
+                'message': response_code[416],
             }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        return Response(response)
