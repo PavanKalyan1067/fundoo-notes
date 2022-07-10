6d76d424a6276c4b36a807bd5982b36bff059f6b
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-11 03:23:29 +0530

    updated updateApiView for Notes

--- FundooNotes/Fundoonotes/views.py
+++ FundooNotes/Fundoonotes/views.py
@@ -1,13 +1,17 @@
+from django.core.cache import cache
+from django.http import Http404
+from django.http.multipartparser import MultiPartParser
 from django.utils import timezone
-from rest_framework import serializers, status, generics, permissions
+from rest_framework import serializers, status, generics, permissions, request
 from rest_framework.exceptions import ValidationError
+from rest_framework.parsers import FormParser, JSONParser
 from rest_framework.response import Response
 from rest_framework_jwt.settings import api_settings
 
 from users.models import User
 from .logger import get_logger
 from .models import Notes
-from Fundoonotes.exceptions import DoesNotExist, validate_time
+from Fundoonotes.exceptions import DoesNotExist, validate_time, DoesNotExistException, PassedTimeException
 from rest_framework.views import APIView
 from .serializers import NotesSerializer
 from Fundoonotes.Response import response_code
@@ -62,7 +66,8 @@ class RetrieveAPIView(APIView):
     def get(self, request, pk):
         try:
             data = Notes.objects.all()
-            # data = get_notes_by_user_id(user_id)
+
+            # data = Notes.objects.get(pk=pk)
             logger.info('Getting the notes data on %s', timezone.now())
             serializer = NotesSerializer(data, many=True)
             serialized_data = serializer.data
@@ -83,43 +88,6 @@ class RetrieveAPIView(APIView):
             response = {
                 'success': False,
                 'message': response_code[416],
-            }
-            logger.exception(e)
-            return Response(response)
-
-
-class UpdateAPIView(APIView):
-    def put(self, request, pk):
-        try:
-            data = Notes.objects.get(pk=pk)
-            serializer = NotesSerializer(data, data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            serialized_data = serializer.data
-            response = {
-                'success': True,
-                'message': response_code[202],
-                'data': {'notes_list': serialized_data},
-            }
-            return Response(response)
-        except Exception as e:
-            response = {
-                'success': False,
-                'message': response_code[416],
-            }
-            logger.exception(e)
-            return Response(response)
-        except ValidationError as e:
-            response = {
-                'success': False,
-                'message': response_code[308],
-            }
-            logger.exception(e)
-            return Response(response)
-        except DoesNotExist as e:
-            response = {
-                'success': False,
-                'message': response_code[307]
             }
             logger.exception(e)
             return Response(response)
@@ -149,3 +117,39 @@ class DeleteAPIView(APIView):
             }
             logger.exception(e)
             return Response(response)
+
+
+class UpdateNotesApiView(generics.GenericAPIView):
+    serializer_class = NotesSerializer
+    data = Notes.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_objects(self, pk):
+        try:
+            return Notes.objects.get(pk=pk)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        note = self.get_objects(pk=pk)
+        serializer = NotesSerializer(note)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        note = self.get_objects(pk=pk)
+        serializer = NotesSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            note = Notes.objects.get(pk=pk)
+            if note.isArchive:
+                note.isPinned = False
+                note.save()
+            response = {
+                'success': True,
+                'message': response_code[200],
+            }
+            return Response(response)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
