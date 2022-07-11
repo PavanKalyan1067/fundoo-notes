35b686d5028057c3294a464db96d257bce18b311
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-12 00:15:48 +0530

    modified NotesSerializer

--- FundooNotes/Fundoonotes/models.py
+++ FundooNotes/Fundoonotes/models.py
@@ -1,19 +1,20 @@
 from django.db import models
-# from django.contrib.auth.models import User
 from users.models import User
-
-# Create your models here.
 from labels.models import Labels
 
 
 class Notes(models.Model):
     title = models.CharField(max_length=500)
     description = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    created_at = models.DateTimeField(auto_created=True, null=True)
-    modified_at = models.DateTimeField(auto_now=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     collaborator = models.ManyToManyField(User, related_name='collaborator')
     label = models.ManyToManyField(Labels)
-    isArchive = models.BooleanField(default=False, )
-    isTrash = models.BooleanField(default=True, )
-    isPinned = models.BooleanField(default=False, )
+    reminder = models.TimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
+    isArchive = models.BooleanField(default=False)
+    isTrash = models.BooleanField(default=False)
+    isPinned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
