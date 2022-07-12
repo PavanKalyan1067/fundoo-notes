a96f403eb7aa9d3a924b33eb306d0c205288a692
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-12 17:23:08 +0530

    added LabelApiView for labels this api will post the labels and get the data for login user

--- FundooNotes/labels/views.py
+++ FundooNotes/labels/views.py
@@ -1,21 +1,45 @@
-from rest_framework import serializers, status
-from django.shortcuts import render
+from rest_framework import serializers, status, generics, permissions
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
-
-# from .logger import get_logger
+from Fundoonotes.Response import response_code
 from labels.models import Labels
-
-# Create your views here.
-from rest_framework.views import APIView
-
 from labels.serializers import LabelSerializer
 
-# logger = get_logger()
 
+class LabelAPIView(generics.GenericAPIView):
+    serializer_class = LabelSerializer
+    queryset = Labels.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
-class RetrieveAPIView(APIView):
+    def post(self, request):
+        user = request.user
+        try:
+            serializer = LabelSerializer(data=request.data, partial=True)
+            serializer.is_valid()
+            serializer.save(user_id=user.id)
+            data = serializer.data
+            response = {
+                'success': True,
+                'message': response_code[201],
+                'data': data
+            }
+            return Response(response)
+
+        except ValidationError as e:
+            response = {
+                'success': False,
+                'message': response_code[308],
+            }
+            return Response(response)
+        except Exception as e:
+            response = {
+                'success': False,
+                'message': response_code[416],
+            }
+            return Response(response)
+
     def get(self, request):
+        user = request.user
         try:
             data = Labels.objects.all()
             serializer = LabelSerializer(data, many=True)
@@ -32,64 +56,3 @@ class RetrieveAPIView(APIView):
                 'message': 'Oops! Something went wrong! Please try again...'
             }
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CreateAPIView(APIView):
-    def post(self, request):
-        try:
-            serializer = LabelSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            labels = Labels(name=serializer.data.get('name'))
-            labels.save()
-            data = serializer.data
-            data.update({'id': labels.id})
-            response = {
-                'success': True,
-                'message': 'Labels created successfully!',
-                'data': {'notes_list': data}
-            }
-            return Response(response, status=status.HTTP_201_CREATED)
-        except Exception as e:
-            response = {
-                'success': False,
-                'message': 'Oops! Something went wrong!'
-            }
-            return Response(response,status=status.HTTP_400_BAD_REQUEST)
-
-
-class UpdateAPIView(APIView):
-    def put(self, request, ):
-        try:
-            data = Labels.objects.get(pk=1)
-            serializer = LabelSerializer(data, data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            serialized_data = serializer.data
-            response = {
-                'status': 'Successfully Updated Data',
-                'user': serialized_data,
-            }
-            return Response(response, status=status.HTTP_202_ACCEPTED)
-        except Exception as e:
-            response = {
-                'success': False,
-                'message': 'Oops! Something went wrong! Please try again...'
-            }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
-
-class DeleteAPIView(APIView):
-    def delete(self, request, ):
-        try:
-            data = Labels.objects.get(pk=9)
-            data.delete()
-            response = {
-                'status': 'Successfully Deleted Data',
-            }
-            return Response(response, status=status.HTTP_202_ACCEPTED)
-        except Exception as e:
-            response = {
-                'success': False,
-                'message': 'Oops! Something went wrong! Please try again...'
-            }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
