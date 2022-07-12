f29b337aa97574f750203b855421a786c9cdbc52
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-12 17:25:58 +0530

    added UpdateLabelsAPIView url

--- FundooNotes/labels/urls.py
+++ FundooNotes/labels/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from labels.views import LabelAPIView
+from labels.views import LabelAPIView, UpdateLabelsAPIView
 
 urlpatterns = [
     path('api/create', LabelAPIView.as_view()),
+    path('api/update/<pk>', UpdateLabelsAPIView.as_view()),
 ]
