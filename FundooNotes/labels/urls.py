1a63edb97e8369a4346686d30bb8ae684c23536a
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-12 17:28:30 +0530

    added DeleteAPIView url

--- FundooNotes/labels/urls.py
+++ FundooNotes/labels/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from labels.views import LabelAPIView, UpdateLabelsAPIView
+from labels.views import LabelAPIView, UpdateLabelsAPIView, DeleteAPIView
 
 urlpatterns = [
     path('api/create', LabelAPIView.as_view()),
     path('api/update/<pk>', UpdateLabelsAPIView.as_view()),
+    path('api/delete/<pk>', DeleteAPIView.as_view()),
+
 ]
