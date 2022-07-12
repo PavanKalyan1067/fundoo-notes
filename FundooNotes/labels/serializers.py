eaf7074076b5d452a0ddd8c2afb546e10c8b7d12
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-12 17:22:46 +0530

    added LabelSerializer for labels

--- FundooNotes/labels/serializers.py
+++ FundooNotes/labels/serializers.py
@@ -4,11 +4,13 @@ from labels.models import Labels
 
 
 class LabelSerializer(serializers.ModelSerializer):
-    """
-        Notes Serializer : name,
-    """
-    name = serializers.CharField(min_length=2, max_length=100, required=True)
+
+    label = serializers.CharField(min_length=2, max_length=100, required=True)
 
     class Meta:
         model = Labels
-        fields = ['name']
+        fields = ['label']
+        read_only_fields = ['id', 'label_id']
+        REQUIRED_FIELDS = ['label']
+
+
