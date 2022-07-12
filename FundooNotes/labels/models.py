2c0be438ab5d218858dabcf75786c331c7526b34
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-12 17:28:50 +0530

    updated models for Labels

--- FundooNotes/labels/models.py
+++ FundooNotes/labels/models.py
@@ -5,7 +5,11 @@ from users.models import User
 # Create your models here.
 
 class Labels(models.Model):
-    name = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    created_at = models.DateTimeField(auto_created=True,null=True)
-    modified_at = models.DateTimeField(auto_now=True,null=True)
+    label = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.label
+
+    REQUIRED_FIELDS = ['label']
+
