da65a845dafff123cbecbaff2be33ac4f46985f4
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-20 17:12:35 +0530

    Refactor:Updated urls in labels

--- FundooNotes/labels/urls.py
+++ FundooNotes/labels/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from labels.views import LabelAPIView, UpdateLabelsAPIView, DeleteAPIView
 
 urlpatterns = [
-    path('api/create', LabelAPIView.as_view()),
-    path('api/update/<pk>', UpdateLabelsAPIView.as_view()),
-    path('api/delete/<pk>', DeleteAPIView.as_view()),
+    path('api/create/', LabelAPIView.as_view()),
+    path('api/update/<pk>/', UpdateLabelsAPIView.as_view()),
+    path('api/delete/<pk>/', DeleteAPIView.as_view()),
 
 ]
