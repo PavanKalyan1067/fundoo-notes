359f9b86c346f04fb3f4b0d60778a096542c886f
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-22 00:11:57 +0530

    Add:Added smtplib for sendemail django email sending

--- FundooNotes/users/utils.py
+++ FundooNotes/users/utils.py
@@ -1,6 +1,9 @@
+import email
+
+from Fundoo import settings
+from django.core.mail import send_mail
+import smtplib
 from django.core.mail import EmailMessage
-
-
 import threading
 
 
@@ -17,6 +20,38 @@ class EmailThread(threading.Thread):
 class Util:
     @staticmethod
     def send_email(data):
+        server = smtplib.SMTP_SSL(settings.EMAIL_HOST, settings.EMAIL_PORT)
+        server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+        msg = f'''
+        {data['email_subject']}
+
+        {data['email_body']}
+        '''
+        server.sendmail(settings.EMAIL_HOST_USER, [data['to_email']], msg)
+        subject = data['email_subject']
+        message = data['email_body'],
+        recipient_list = [data['to_email']]
+        from_email = settings.EMAIL_HOST_USER
+        send_mail(subject, message, recipient_list=recipient_list, from_email=from_email)
+
+    @staticmethod
+    def django_email(data):
         email = EmailMessage(
-            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
+            subject=data['email_subject'], body=data['email_body'], from_email=data['from_email'],
+            to=[data['to_email']])
+        # server.quit()
         EmailThread(email).start()
+# class Util:
+#     @staticmethod
+#     def send_email(data):
+#         email = EmailMessage(
+#             subject=data['subject'],
+#             body=data['body'],
+#             from_email='princeabhi966@gmail.com',
+#             to=['abhilashmeher1234@gmail.com']
+#         )
+#         email.send()
+
+
+# if __name__ == '__main__':
+#     Util.send_email({'email_subject': 'abc', 'email_body': 'hey', 'to_email': 'pavan.kal98@gmail.com'})
