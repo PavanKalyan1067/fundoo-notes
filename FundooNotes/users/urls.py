44c203db5796c7fa865389c241136d895f6e35e9
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-22 00:47:02 +0530

    Refactored urls of users

--- FundooNotes/users/urls.py
+++ FundooNotes/users/urls.py
@@ -11,13 +11,13 @@ from users.views import (
 from rest_framework_simplejwt.views import (TokenRefreshView, )
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name="register"),
-    path('login/', LoginAPIView.as_view(), name="login"),
-    path('logout/', LogoutAPIView.as_view(), name="logout"),
-    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('forgot-password/', ForgotPasswordResetEmailAPIView.as_view(),name='forgot-password-reset'),
-    path('reset-password/<uid>/<token>/', SetNewPasswordAPIView.as_view(), name='reset-password'),
-    path('get-all-users/', UserProfileView.as_view(), name="all_users"),
+    path('api/register/', RegisterView.as_view(), name="register"),
+    path('api/login/', LoginAPIView.as_view(), name="login"),
+    path('api/logout/', LogoutAPIView.as_view(), name="logout"),
+    path('api/email-verify/', VerifyEmail.as_view(), name="email-verify"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/forgot-password/', ForgotPasswordResetEmailAPIView.as_view(),name='forgot-password-reset'),
+    path('api/reset-password/<uid>/<token>/', SetNewPasswordAPIView.as_view(), name='reset-password'),
+    path('api/get-all-users/', UserProfileView.as_view(), name="all_users"),
 
 ]
