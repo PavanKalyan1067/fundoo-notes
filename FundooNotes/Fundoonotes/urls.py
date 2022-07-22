4ee001b5529bc04035f3c2e360df7ad10b6ac155
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-23 02:02:22 +0530

    Add:Added more urls for notes app

--- FundooNotes/Fundoonotes/urls.py
+++ FundooNotes/Fundoonotes/urls.py
@@ -10,19 +10,25 @@ from Fundoonotes.views import (
     TrashNotesAPIView,
     AllPinNotesAPIView,
     PinNotesAPIView,
+    DisplayNoteByLabelView,
+    CollaboratedNoteView,
+    LabelNoteView,
 
 )
 
 urlpatterns = [
-    path('api/retrieve/', RetrieveAPIView.as_view()),
-    path('api/update/<int:pk>/', UpdateNotesAPIView.as_view()),
-    path('api/delete/<pk>/', DeleteAPIView.as_view()),
-    path('api/create/', CreateAPIView.as_view()),
-    path('api/archive/<pk>', ArchiveNotesAPIView.as_view()),
-    path('api/archive1/', AllArchiveNotesAPIView.as_view()),
-    path('api/trash/', AllTrashNotesAPIView.as_view()),
-    path('api/trash1/<pk>', TrashNotesAPIView.as_view()),
-    path('api/pin/<pk>', PinNotesAPIView.as_view()),
-    path('api/pin1/', AllPinNotesAPIView.as_view()),
+    path('api/retrieve/', RetrieveAPIView.as_view(), name="retrieve"),
+    path('api/update/<int:pk>/', UpdateNotesAPIView.as_view(), name="update-notes"),
+    path('api/delete/<int:pk>/', DeleteAPIView.as_view(), name="delete-notes"),
+    path('api/create/', CreateAPIView.as_view(), name="create-notes"),
+    path('api/archive/<int:pk>', ArchiveNotesAPIView.as_view(), name="archive"),
+    path('api/all-archive-notes/', AllArchiveNotesAPIView.as_view(), name="all-archive-notes"),
+    path('api/all-trash-notes/', AllTrashNotesAPIView.as_view(), name="all-trash-notes"),
+    path('api/trash/<int:pk>', TrashNotesAPIView.as_view(), name="trash"),
+    path('api/pin/<int:pk>', PinNotesAPIView.as_view(), name="pin"),
+    path('api/all-pin-notes/', AllPinNotesAPIView.as_view(), name="all-pin-notes"),
+    path('api/display/<label>', DisplayNoteByLabelView.as_view(), name="display"),
+    path('api/collaborated/note/', CollaboratedNoteView.as_view(), name="collaborated-note"),
+    path('api/label/note/', LabelNoteView.as_view(), name="label-note"),
 
 ]
