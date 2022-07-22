6f7899bad64da02de4337b671429f2f8192a4123
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-07-23 01:59:40 +0530

    Add:Added Swagger in Fundoo Urls

--- FundooNotes/Fundoo/urls.py
+++ FundooNotes/Fundoo/urls.py
@@ -1,26 +1,17 @@
-"""Fundoo URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from .routers import router
 
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Fundoo Notes')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('fundoonotes/', schema_view),
+    path('user/', include('users.urls')),
     path('notes/', include('Fundoonotes.urls')),
     path('labels/', include('labels.urls')),
-    path('api/notes/', include(router.urls)),
-    path('api/user/', include('users.urls')),
+    # path('api/notes/', include(router.urls)),
 ]
